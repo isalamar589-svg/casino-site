d6a76fb5def8e281dc93fe824eb34714042a80e7
isalamar589-svg <isalamar589@gmail.com>
2026-09-04 19:36:46 +0600

    Initial casino commit

---
+++ core/sql.php
@@ -0,0 +1,8 @@
+<?php
+	define("DB_HOST", "gateway01.ap-southeast-1.prod.aws.tidbcloud.com");
+	define("DB_PORT", 4000);
+	define("DB_USER", "G8w7sVWNh89zo1h.root");
+	define("DB_PASS", "OboFM7016XoTC6TZ");
+	define("DB_NAME", "test");
+?>
+
